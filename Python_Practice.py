cab65f67a5e9ab5726356a19ed1b786a2de04c2f
Samarth Katkar <samarthkatkar69@gmail.com>
2026-09-02 22:29:50 +0530

    Update Python_Practice.py

--- Python_Practice.py
+++ Python_Practice.py
@@ -1,3 +1,5 @@
+# 1/8/26
+
 # i=1
 # for i in range(1,11):
 #     print(f"3 X {i} = {i*3}")
@@ -17,6 +19,8 @@
 # age = int(input("Enter your age:"))  
 # print(f"My name is {name} and my age is {age}")              
 
+# 2/8/26
+
 # for i in range(1,6):
 #     if i==3:
 #         break //break the loop and exit the loop
@@ -35,6 +39,50 @@
 # tab button is used to indent the code in python.
 # and shift+tab is used to unindent the code in python.  
 
-                     
+# ''' is uded to write multi line comments in python.
+
+# name="Samarth"
+# print(name[2])
+
+# name = "Samarth"
+# for i in name:
+#     print(i)
+
+# name = "S a m a r t h" // strings are immutable in python.
+# #       0 1 2 3 4 5 6
+# print(name[-1])
+# print(name[-2])
+# print(name[-3])
+# print(name[-4])
+
+# name="samarth katkar"
+# a=name.upper()
+# print(a)
+# b=name.lower()
+# print(b)
+# print(name.title()) # title() is used to capitalize the first letter of each word in a string.
+# print(name.capitalize()) # capitalize() is used to capitalize the first letter of the string.
+# print(name.strip()) # strip() is used to remove the leading and trailing spaces from a string.
+# print(name.lstrip()) # lstrip() is used to remove the leading spaces from a string.
+# print(name.rstrip()) # rstrip() is used to remove the trailing spaces from a string.
                                                                                                             
- 
+# fruits= "apple,banana,guava,pineapple"
+# a= fruits.split(",")
+# print(a)
+# b="-".join(a)
+# print(b)
+# print(len(fruits))
+
+# marks=[23,21,15,10,9,22,12,11]
+# #print(marks[0:3])
+# marks.append(20)#to add a number at the end of the list
+# marks.insert(1,4) #to insert a number at specific position
+# print(marks)
+# marks.remove(23) #to remove a number from the list
+# print(marks)
+# marks.pop() #to remove the last number from the list
+# print(marks)
+# marks.reverse() #to reverse the list
+# print(marks)
+# marks.sort() #to sort the list in ascending order
+# print(marks)
