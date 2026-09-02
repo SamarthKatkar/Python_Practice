71cdd569203f5e6b576e75110147869749a7e881
Samarth Katkar <samarthkatkar69@gmail.com>
2026-09-02 20:58:49 +0530

    Add files via upload

---
+++ Python_Practice.py
@@ -0,0 +1,40 @@
+# i=1
+# for i in range(1,11):
+#     print(f"3 X {i} = {i*3}")
+
+# i=1
+# while i<=6:
+#     print(i)
+#     i +=1
+
+# a=int(input("Enter your age:"))
+# # print(a)
+# if a<=18:
+#     print("Not eligible")
+# else:
+#     print("Eligible")
+# name = input("Enter your name:")
+# age = int(input("Enter your age:"))  
+# print(f"My name is {name} and my age is {age}")              
+
+# for i in range(1,6):
+#     if i==3:
+#         break //break the loop and exit the loop
+#     print(i)
+
+# for i in range(1,6):
+#     if i==3:
+#         continue //dont execute the code below and continue with the next iteration
+#     print(i)
+
+# for i in range(1,6):
+#     if i==3:
+#         pass //do nothing and continue with the next iteration
+#     print(i)
+                    
+# tab button is used to indent the code in python.
+# and shift+tab is used to unindent the code in python.  
+
+                     
+                                                                                                            
+ 
